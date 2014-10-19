1e6aaa27369d47941f76be310a2f08956d1e9564
wovvan <efanov1993@gmail.com>
2014-10-19 16:13:34 +0400

    Добавил проверку на нахождение корабля в порту, и добавил вместиомсть кораблю

--- ships.rb
+++ ships.rb
@@ -1,54 +1,74 @@
 class Ship
-	attr_accessor :name, :water_out, :zagr
+  attr_accessor :name, :water_out, :zagr, :kind
 
-	def initialize name, water_out, kind = "unknown"
-		@name = name || ''
-		@water_out = water_out || 0
+  def initialize name = '', water_out = 0, kind = "unknown", vmestimost = 0
+    @name = name
+    @water_out = water_out
     @zagr = false
     @kind = kind
-	end
-	def zagruzka weight 
-		count = 0
-		while count < weight
-			@water_out += 10
-			puts water_out
-			count += 10
-			sleep(1)
-		end
-		@zagr = true
-	end
-	def razgruzka
-		@zagr = false
-	end  
-	def state
-		if(zagr) then "загружен" else "не загружен" end
-	end
+    @vmestimost = vmestimost
+  end
+  def zagruzka weight 
+    count = 0
+    while count < weight
+      @water_out += 10
+      puts water_out
+      count += 10
+      sleep(1)
+    end
+    @zagr = true
+  end
+  def razgruzka
+    @zagr = false
+  end  
+  def state
+    if(zagr) then "загружен" else "не загружен" end
+  end
+  def ship_vmestimost
+    if(@kind == 'P')
+        p @vmestimost.to_s + ' количество пассажиров'
+      elsif (@kind == 'G')
+          p @vmestimost.to_s + ' тонн груза'
+      else p @vmestimost.to_s + ' вместимость чего-то'
+    end
+  end
 end
+
 class Port
-	attr_accessor :name, :ships
-	def initialize name
-		@name = name
-		@ships = []
-	end
-	def priem ship
-        @ships << ship
-	end
-	def otpravka ship
+  attr_accessor :name, :ships
+  def initialize name
+    @name = name
+    @ships = []
+  end
+  def priem ship
+    if (!@ships.include?(ship))
+      @ships << ship
+    else
+      p ship.name + ": такой корабль уже есть"
+    end
+  end
+  def otpravka ship
         @ships.delete ship
-	end
-	def print_ships 
-		@ships.each do |ship|
-			puts ship.name
-		end
-	end
+  end
+  def print_ships 
+    @ships.each do |ship|
+      puts ship.name
+    end
+  end
 end
-ship = Ship.new("avrora", 100, "G")
-ship2 = Ship.new("avrora2", 100, "P")
-ship3 = Ship.new("avrora3", 100)
+ship = Ship.new("avrora", 100, "G", 300)
+ship2 = Ship.new("avrora2", 100, "P", 150)
+ship3 = Ship.new("avrora3", 100, "?", 100)
 port = Port.new("artur")
-p ship
-p ship2
-p ship3
+port.priem(ship)
+port.priem(ship2)
+port.priem(ship3)
+port.priem(ship3)
+port.print_ships
+puts "*************"
+ship.ship_vmestimost
+ship2.ship_vmestimost
+ship3.ship_vmestimost
 # port.priem(ship)
 # port.priem(ship2)
 # port.priem(ship2)
