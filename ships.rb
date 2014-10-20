301fe4916c7a6ca6a89f6361295585abc4808896
wovvan <efanov1993@gmail.com>
2014-10-20 10:45:53 +0400

    у кораблей появилось свойство порт

--- ships.rb
+++ ships.rb
@@ -1,12 +1,13 @@
 class Ship
-  attr_accessor :name, :water_out, :zagr, :kind
+  attr_accessor :name, :water_out, :zagr, :kind, :port
 
-  def initialize name = '', water_out = 0, kind = "unknown", vmestimost = 0
+  def initialize name = '', water_out = 0, kind = "unknown", vmestimost = 0, port
     @name = name
     @water_out = water_out
     @zagr = false
     @kind = kind
     @vmestimost = vmestimost
+    @port = port
   end
   def zagruzka weight 
     count = 0
@@ -35,30 +36,31 @@ class Ship
 end
 
 class Port
-  attr_accessor :name, :ships, :ship_count, :vmestimost
+  attr_accessor :name, :ships, :vmestimost
   def initialize name, vmestimost
     @name = name
     @ships = []
     @vmestimost = vmestimost
-    @ship_count = 0
   end
   def priem ship
-    if (!@ships.include?(ship))
-      if(@ship_count == vmestimost)
-        p "Порт " + @name.to_s + " полон"
-        else
-          @ships << ship
-          @ship_count += 1
-          p "Порт " + @name.to_s + " принял: " + ship.name
-      end 
+    if ship.port == self    
+      if (!@ships.include?(ship))
+        if(@ships.count == vmestimost)
+          p "Порт " + @name.to_s + " полон"
+          else
+            @ships << ship
+            p "Порт " + @name.to_s + " принял: " + ship.name
+        end 
+      else
+        p ship.name + ": такой корабль уже есть"
+      end
     else
-      p ship.name + ": такой корабль уже есть"
+      p "корабль " + ship.name + " не может быть прянят, он следует в порт " + ship.port.name
     end
   end
   def otpravka ship
       if (@ships.include?(ship))
         @ships.delete ship
-        @ship_count -= 1
         p "коробль " + ship.name + " отправлен"
       else
         p "коробля " + ship.name + " в порту нет"
@@ -70,16 +72,19 @@ class Port
     end
   end
 end
-ship = Ship.new("avrora", 100, "G", 300)
-ship2 = Ship.new("avrora2", 100, "P", 150)
-ship3 = Ship.new("avrora3", 100, "?", 100)
-ship4 = Ship.new("avrora4", 200, "P", 1100)
-port = Port.new("artur", 2)
+port = Port.new("artur", 4)
+port2 = Port.new("pearl-harbor", 4)
+ship = Ship.new("avrora", 100, "G", 300, port)
+ship2 = Ship.new("avrora2", 100, "P", 150, port2)
+ship3 = Ship.new("avrora3", 100, "?", 100, port)
+ship4 = Ship.new("avrora4", 200, "P", 1100, port2)
+ship5 = Ship.new("avrora5", 200, "P", 1100, port)
 port.priem(ship)
-port.priem(ship2)
+port2.priem(ship2)
 port.priem(ship3)
 port.otpravka(ship)
-port.priem(ship4)
+port2.priem(ship4)
+port2.priem(ship5)
 port.print_ships
 puts "*************"
 # port.priem(ship)
